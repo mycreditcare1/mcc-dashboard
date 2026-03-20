08b5fa0f97283f069eff92d9f3ada0347c8d46fb
mycreditcare1 <alex@mycreditcare.org>
2026-03-19 23:06:34 -0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,7 +1,3 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    agents: Math.floor(Math.random() * 10),
-    calls: Math.floor(Math.random() * 5),
-    leads: Math.floor(Math.random() * 200)
-  });
-}
+import handler from './ghl';
+
+export default handler;
